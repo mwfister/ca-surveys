20ca9704e6e5c00eee38f62e399b4838469ddcb5
mwfister <mwfister@gmail.com>
2018-08-09 12:52:56 +1000

    Made survey actions more granular
    Fetching surveys will be async, so produced actions to track status (for UX - loading indicator).

--- client/src/actions/surveys.js
+++ client/src/actions/surveys.js
@@ -1,11 +1,30 @@
-const RECEIVE_SURVEYS = 'RECEIVE_SURVEYS'
+const FETCH_SURVEYS_REQUEST = 'FETCH_SURVEYS_REQUEST'
+const FETCH_SURVEYS_SUCCESS = 'FETCH_SURVEYS_SUCCESS'
+const FETCH_SURVEYS_FAILURE = 'FETCH_SURVEYS_ERROR'
 
-export function receiveSurveys({ surveys, themes, questions, respondents }) {
+export function fetchSurveysRequest() {
   return {
-    type: RECEIVE_SURVEYS,
+    type: FETCH_SURVEYS_REQUEST,
+  }
+}
+
+export function fetchSurveysSuccess({ surveys, themes, questions, respondents }) {
+  return {
+    type: FETCH_SURVEYS_SUCCESS,
     surveys,
     themes,
     questions,
     respondents,
   }
+}
+
+export function fetchSurveysFailure(error) {
+  return {
+    type: FETCH_SURVEYS_FAILURE,
+    error,
+  }
+}
+
+export function handleInitialData() {
+
 }

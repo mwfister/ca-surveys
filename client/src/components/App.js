53802ebe7b422703efce8e820ec197d35b872895
mwfister <mwfister@gmail.com>
2018-08-10 17:25:52 +1000

    [Testing] normalizing fetched data

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react';
 
+import { normalize } from 'normalizr'
+import surveysSchema from '../utils/schema'
+import { log } from '../utils/helpers'
+
 class App extends Component {
+
   render() {
-    fetch('/api/surveys')
+    fetch('/api/survey_results/1.json')
       .then((response) => response.json())
-      .then((jsond) => console.log(jsond))
+      .then(log('API response in JSON'))
+      .then(({ survey_result_detail: survey }) => {
+        console.log("data", survey );
+        const normalisedState = normalize(survey, surveysSchema)
+
+        console.group("State")
+        console.log("Normalised state", normalisedState);
+        console.log(JSON.stringify(normalisedState));
+        console.groupEnd()
+        return normalisedState
+      })
       .catch((e) => console.error("error", e))
 
     return (

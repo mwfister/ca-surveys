a9dd00552979f4bbc6aa95c1e1af645f24559c2c
mwfister <mwfister@gmail.com>
2018-08-14 12:45:15 +1000

    Adding redux store to the App.
    Currently only uses one reducer, combine reducers would need to be implemented for additonal reducers.

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 import './index.css';
+
 import App from './components/App';
+import reducers from './reducers/surveys'
+
+const store = createStore(reducers)
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store} >
+    <App />
+  </Provider>,
   document.getElementById('root')
 );

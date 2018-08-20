6aa70ea96053a37bb2e9d620c11a83d128eb6a47
mwfister <mwfister@gmail.com>
2018-08-20 17:36:56 +1000

    Survey Card component
    For Dashboard rendering

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class Card extends Component {
+import { fetchSurveyData } from '../actions/surveys'
+
+class Card extends Component {
+  componentDidMount() {
+    this.props.dispatch(fetchSurveyData(`${this.props.survey.url}`))
+  }
+
   render() {
+    const {
+      name,
+      participant_count,
+      response_rate,
+    } = this.props.survey
+    const response_percentage = `${Math.round(response_rate*100)}%`
+
     return (
-      <div>Hi</div>
+      // TODO: Router Link to survey page
+      <div className="card">
+        <h2>{name}</h2>
+        <div>
+          <em>{response_percentage}</em>
+          <p>Participation</p>
+        </div>
+        <div>from {participant_count} respondants</div>
+      </div>
     )
   }
 }
+
+export default connect()(Card)

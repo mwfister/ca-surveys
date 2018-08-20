1f86ed09aeba01edcdb6e0f3094031a4bdcd023c
mwfister <mwfister@gmail.com>
2018-08-20 17:37:35 +1000

    Basic styling for UI

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,10 +15,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+        <div className="container">
           <Dashboard />
-        </header>
         </div>
       </div>
     )

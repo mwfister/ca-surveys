1e7d3c3a3ccb2d005460df215479cac0d65efa35
mwfister <mwfister@gmail.com>
2018-09-03 11:55:56 +1000

    Temporary loading survey index

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,19 +4,24 @@ import { connect } from 'react-redux'
 import surveysSchema from '../utils/schema'
 import { handleInitialData, fetchSurveyData } from '../actions/surveys'
 import Dashboard from './Dashboard'
+import Survey from './Survey'
 
 class App extends Component {
-  componentDidMount() {
+  componentWillMount() {
     this.props.dispatch(handleInitialData())
-    //this.props.dispatch(fetchSurveyData('/survey_results/1.json'))
-    //this.props.dispatch(fetchSurveyData('/survey_results/2.json'))
+  }
+  componentDidMount() {
+    this.props.dispatch(fetchSurveyData('/survey_results/1.json'))
+    this.props.dispatch(fetchSurveyData('/survey_results/2.json'))
   }
 
   render() {
     return (
       <div className="App">
         <div className="container">
-          <Dashboard />
+          {this.props.loading ? <Survey surveyIndex={0}/> :
+          <p>Loading</p>
+          }
         </div>
       </div>
     )
@@ -27,7 +32,8 @@ const mapStateToProps = (state) => {
   //const { surveys, themes, questions, respondents } = state
 
   return {
-    surveys: state.surveys
+    surveys: state.surveys,
+    loading: state.surveys[0],
     //themes,
     //questions,
     //respondents,
